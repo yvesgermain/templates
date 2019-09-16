8fdec42ea83eec50f2e9aeb69bc8c2d1cbabac41
Yves Germain <ygermain@soquij.local>
2019-09-16 11:38:48 -0400

    Copie la variable env:Environnement dans le fichier c:\temp\environnement.txt

--- GumStorage-rg-prd/Azcopy_Storage.ps1
+++ GumStorage-rg-prd/Azcopy_Storage.ps1
@@ -33,7 +33,7 @@ $ErrorActionPreference = "Stop"
 #Create or check for existing resource group
 $ResourceGroupName = "gumstorage-rg-" + $environnement
 
-env:Environnement > C:\temp\Environnement.txt
+env:Environnement > C:\testresults\Environnement.txt
 
 get-azureRMstorageaccount -resourcegroupName $resourceGroupName | where-object { $_.storageaccountname -like "storgum*" } | foreach-object { 
     $name = $_.storageaccountname; 
