9e30a569f4c932631f77d2f5ba505fe297268f1e
Yves Germain <ygermain@soquij.local>
2019-09-17 17:05:00 -0400

    Correction du fichier Copy-file.ps1

--- Storage-rg-prd/Copy-file.ps1
+++ Storage-rg-prd/Copy-file.ps1
@@ -12,7 +12,7 @@ get-azureRmstorageaccount -resourcegroupName $resourceGroupName | where-object {
     $name = $_.storageaccountname; 
     Get-azureRMStorageAccountKey -ResourceGroupName $_.resourcegroupname -Name $_.StorageAccountName } | where-object { $_.keyname -like "key1"} | ForEach-Object { 
         $Secret = ConvertTo-SecureString -String $_.value -AsPlainText -Force; 
-        Set-azureRmKeyVaultSecret -VaultName 'gumkeyvault' -Name $name -SecretValue $Secret -ContentType "Storage key"
+        Set-azureKeyVaultSecret -VaultName 'gumkeyvault' -Name $name -SecretValue $Secret -ContentType "Storage key"
     }
 
 get-azureRmstorageaccount -resourcegroupName $resourceGroupName | where-object {$_.storageaccountname -like "storveillefunc*"} | foreach-object { 
