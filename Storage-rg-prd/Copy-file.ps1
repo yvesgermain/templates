e3ed017d5edc54598aa6e060f2c4f5f17bfa28ac
Yves Germain <ygermain@soquij.local>
2019-09-17 16:58:42 -0400

    Correction du fichier Copy-file.ps1

--- Storage-rg-prd/Copy-file.ps1
+++ Storage-rg-prd/Copy-file.ps1
@@ -10,20 +10,20 @@ $resourceGroupName = "Storage-rg-$environnement"
 
 get-azureRmstorageaccount -resourcegroupName $resourceGroupName | where-object {$_.storageaccountname -like "storappsinterne*"} | foreach-object { 
     $name = $_.storageaccountname; 
-    Get-azureRmStorageAccountKey -ResourceGroupName $_.resourcegroupname -Name $_.StorageAccountName } | where-object { $_.keyname -like "key1"} | ForEach-Object { 
+    Get-azureStorageAccountKey -ResourceGroupName $_.resourcegroupname -Name $_.StorageAccountName } | where-object { $_.keyname -like "key1"} | ForEach-Object { 
         $Secret = ConvertTo-SecureString -String $_.value -AsPlainText -Force; 
         Set-azureRmKeyVaultSecret -VaultName 'gumkeyvault' -Name $name -SecretValue $Secret -ContentType "Storage key"
     }
 
 get-azureRmstorageaccount -resourcegroupName $resourceGroupName | where-object {$_.storageaccountname -like "storveillefunc*"} | foreach-object { 
     $name = $_.storageaccountname; 
-    Get-azureRmStorageAccountKey -ResourceGroupName $_.resourcegroupname -Name $_.StorageAccountName } | where-object { $_.keyname -like "key1"} | ForEach-Object { 
+    Get-azureStorageAccountKey -ResourceGroupName $_.resourcegroupname -Name $_.StorageAccountName } | where-object { $_.keyname -like "key1"} | ForEach-Object { 
         $Secret = ConvertTo-SecureString -String $_.value -AsPlainText -Force; 
         Set-azureKeyVaultSecret -VaultName 'gumkeyvault' -Name $name -SecretValue $Secret -ContentType "Storage key"
     }
     
 
-$SourceKey = (get-azureRmstorageaccountkey -Name storappsinterneprd -ResourceGroupName storage-rg-prd | where-object {$_.keyname -eq "key1"}).value
-$DestKey   = (get-azureRmstorageaccountkey -Name storappsinterne$Environnement -ResourceGroupName $resourceGroupName | where-object {$_.keyname -eq "key1"}).value
+$SourceKey = (Get-azureStorageAccountKey -Name storappsinterneprd -ResourceGroupName storage-rg-prd | where-object {$_.keyname -eq "key1"}).value
+$DestKey   = (Get-azureStorageAccountKey -Name storappsinterne$Environnement -ResourceGroupName $resourceGroupName | where-object {$_.keyname -eq "key1"}).value
 
 . $AzCopyPath /source:https://storappsinterneprd.blob.core.windows.net/appsinterne/ /sourcekey:$SourceKey /dest:https://storappsinterne$Environnement.blob.core.windows.net/appsinterne/ /s /y /destkey:$destkey
