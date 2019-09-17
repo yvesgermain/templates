b432de7c387a723a53736c2eb6841e546311f362
Yves Germain <ygermain@soquij.local>
2019-09-17 15:24:17 -0400

    Creation du fichier Copy-file.ps1

---
+++ Storage-rg-prd/Copy-file.ps1
@@ -0,0 +1,29 @@
+param(
+ [Parameter(Mandatory=$True)]
+ [string]
+ [ValidateSet("dev", "qa", "prd", "devops")]
+ $Environnement
+)
+
+$AzCopyPath = "C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy\AzCopy.exe"
+$resourceGroupName = "Storage-rg-$environnement"
+
+get-azureRmstorageaccount -resourcegroupName $resourceGroupName | where-object {$_.storageaccountname -like "storappsinterne*"} | foreach-object { 
+    $name = $_.storageaccountname; 
+    Get-azureRmStorageAccountKey -ResourceGroupName $_.resourcegroupname -Name $_.StorageAccountName } | where-object { $_.keyname -like "key1"} | ForEach-Object { 
+        $Secret = ConvertTo-SecureString -String $_.value -AsPlainText -Force; 
+        Set-azureRmKeyVaultSecret -VaultName 'gumkeyvault' -Name $name -SecretValue $Secret -ContentType "Storage key"
+    }
+
+get-azureRmstorageaccount -resourcegroupName $resourceGroupName | where-object {$_.storageaccountname -like "storveillefunc*"} | foreach-object { 
+    $name = $_.storageaccountname; 
+    Get-azureRmStorageAccountKey -ResourceGroupName $_.resourcegroupname -Name $_.StorageAccountName } | where-object { $_.keyname -like "key1"} | ForEach-Object { 
+        $Secret = ConvertTo-SecureString -String $_.value -AsPlainText -Force; 
+        Set-azureKeyVaultSecret -VaultName 'gumkeyvault' -Name $name -SecretValue $Secret -ContentType "Storage key"
+    }
+    
+
+$SourceKey = (get-azureRmstorageaccountkey -Name storappsinterneprd -ResourceGroupName storage-rg-prd | where-object {$_.keyname -eq "key1"}).value
+$DestKey   = (get-azureRmstorageaccountkey -Name storappsinterne$Environnement -ResourceGroupName $resourceGroupName | where-object {$_.keyname -eq "key1"}).value
+
+. $AzCopyPath /source:https://storappsinterneprd.blob.core.windows.net/appsinterne/ /sourcekey:$SourceKey /dest:https://storappsinterne$Environnement.blob.core.windows.net/appsinterne/ /s /y /destkey:$destkey
