7e2a25c8dd6cde9cc13f51ff4cc2d4151fbb2e52
Yves Germain <ygermain@soquij.local>
2019-09-18 09:31:13 -0400

    Correction de copy-database.ps1

--- SQLApps-rg-prd/Copy-Database.ps1
+++ SQLApps-rg-prd/Copy-Database.ps1
@@ -4,11 +4,12 @@ param(
   $Environnement
 )
 
-import-module az.sql 
-Import-Module Az.KeyVault
+import-module azurerm.sql 
+import-module SQLPS
+Import-Module Azurerm.KeyVault
 $environnement = $resourceGroupName.split("-")[-1]
 $username = "sqladmin$environnement@sqlguminterne-$environnement.database.windows.net"
-$password = (Get-AzKeyVaultSecret -VaultName gumkeyvault -Name sqladmin$environnement  ).SecretValue
+$password = (Get-AzureKeyVaultSecret -VaultName gumkeyvault -Name sqladmin$environnement  ).SecretValue
 $Cred = New-Object System.Management.Automation.PSCredential -ArgumentList ($username, $password)
 
 # invoke-sqlcmd -ServerInstance sqlguminterne-devops.database.windows.net -Database BdAppsInterne-devops -Query "select @@version" -Credential $cred
@@ -26,12 +27,12 @@ foreach( $Bd in $Bdarray) {
     $TargetResourceGroupName = "sqlapps-rg-" +  $Environnement
 
     "Removing database $TargetDatabaseName"
-    if (get-azSqlDatabase -DatabaseName $TargetDatabaseName -ServerName $TargetServerName -ResourceGroupName $TargetResourceGroupName -ErrorAction SilentlyContinue)
+    if (get-azurermDatabase -DatabaseName $TargetDatabaseName -ServerName $TargetServerName -ResourceGroupName $TargetResourceGroupName -ErrorAction SilentlyContinue)
 {
-    Remove-azSqlDatabase -DatabaseName $TargetDatabaseName -ServerName $TargetServerName -ResourceGroupName $TargetResourceGroupName
+    Remove-azurermSqlDatabase -DatabaseName $TargetDatabaseName -ServerName $TargetServerName -ResourceGroupName $TargetResourceGroupName
     }
     "Copying database $databaseName from server $servername to database $TargetDatabaseName on $TargetServerName"
-    New-azSqlDatabaseCopy -ServerName $serverName -ResourceGroupName $resourceGroupName -DatabaseName $databaseName `
+    New-azureRmSqlDatabaseCopy -ServerName $serverName -ResourceGroupName $resourceGroupName -DatabaseName $databaseName `
     -CopyResourceGroupName $TargetResourceGroupName -CopyServerName $TargetServerName -CopyDatabaseName $TargetDatabaseName
  }
 
