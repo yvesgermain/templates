49bf80ede325ff4e62c333762526e9d5510733a6
Charbonneau, Philippe <pcharbonneau@soquij.qc.ca>
2019-09-30 09:30:59 -0400

    ajout du chromeinstall a l'intérieur du script

--- AppsCrawler-rg-prd/crawler.ps1
+++ AppsCrawler-rg-prd/crawler.ps1
@@ -83,7 +83,23 @@ if ($arrayList.ipAddress -notcontains ($Ip + '/32')) {
     $WebAppConfig | Set-AzureRMResource -ApiVersion $APIVersion -Force -Verbose
 }
 "Configurer la vm avec Chrome et installer le crawler"
-Invoke-AzureRMVMRunCommand -ResourceGroupName $ResourceGroupName -Name $VmName -CommandId 'RunPowerShellScript' -ScriptPath ".\Install-chrome.ps1" -Parameter @{"Environnement" = $Environnement}
+
+
+$LocalTempDir = $env:TEMP; 
+"Starting";
+$ChromeInstaller = "ChromeInstaller.exe"; 
+(new-object    System.Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', "$LocalTempDir\$ChromeInstaller");
+& "$LocalTempDir\$ChromeInstaller" /silent /install;
+"Downloading TriggerExecCrawler.zip"
+(new-object System.Net.WebClient).DownloadFile('https://gumbackups.blob.core.windows.net/depot-tfs/TriggerExecCrawler.zip', "$env:temp\TriggerExecCrawler.zip");
+"Decompressing file TriggerExecCrawler.zip in c:\crawler"
+Expand-Archive -LiteralPath "$env:temp\TriggerExecCrawler.zip" -DestinationPath C:\crawler
+Get-ChildItem C:\crawler\*\ControleQualite.App.exe | foreach-object {set-location $_.DirectoryName}
+(Get-Content ControleQualite.App.exe.config ).replace('gummaster-dev' , "gummaster-$environnement") | set-content .\ControleQualite.App.exe.config -Encoding UTF8
+.\ControleQualite.App.exe
+"Done!";
+
+
 "Retirer les droits sur le blob https://gumbackups.blob.core.windows.net/depot-tfs"
 Get-AzureStorageContainer depot-tfs -Context $storageContext | set-AzureRMstorageContainerAcl -Permission  Off
 
