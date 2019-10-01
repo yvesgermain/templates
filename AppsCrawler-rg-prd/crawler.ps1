0fc082fef9ceeb4da08105572dcb03477702e0e1
Charbonneau, Philippe <pcharbonneau@soquij.qc.ca>
2019-10-01 08:58:08 -0400

    Debug

--- AppsCrawler-rg-prd/crawler.ps1
+++ AppsCrawler-rg-prd/crawler.ps1
@@ -35,54 +35,54 @@ if ( get-AzureRMResourceGroup -Name $ResourceGroupName -Location $Location -Erro
 }
  while (get-AzureRMResourceGroup -Name $ResourceGroupName -Location $Location -ErrorAction SilentlyContinue ) { start-sleep 20}
 
-#  "Création du resourceGroup $ResourceGroupName"
-# New-AzureRMResourceGroup -Name $ResourceGroupName -Location $Location -Tag @{"Environnement" = $environnement } 
-# $Vnet = New-AzureRMVirtualNetwork -Name $NetworkName -ResourceGroupName $ResourceGroupName -Location $Location -AddressPrefix $VnetAddressPrefix -Subnet $SingleSubnet
+ "Création du resourceGroup $ResourceGroupName"
+New-AzureRMResourceGroup -Name $ResourceGroupName -Location $Location -Tag @{"Environnement" = $environnement } 
+$Vnet = New-AzureRMVirtualNetwork -Name $NetworkName -ResourceGroupName $ResourceGroupName -Location $Location -AddressPrefix $VnetAddressPrefix -Subnet $SingleSubnet
 
-# $PIP = New-AzureRMPublicIpAddress -Name $PublicIPAddressName -DomainNameLabel $DNSNameLabel -ResourceGroupName $ResourceGroupName -Location $Location -AllocationMethod Dynamic
-# $NIC = New-AzureRMNetworkInterface -Name $NICName -ResourceGroupName $ResourceGroupName -Location $Location -SubnetId $Vnet.Subnets[0].Id -PublicIpAddressId $PIP.Id
+$PIP = New-AzureRMPublicIpAddress -Name $PublicIPAddressName -DomainNameLabel $DNSNameLabel -ResourceGroupName $ResourceGroupName -Location $Location -AllocationMethod Dynamic
+$NIC = New-AzureRMNetworkInterface -Name $NICName -ResourceGroupName $ResourceGroupName -Location $Location -SubnetId $Vnet.Subnets[0].Id -PublicIpAddressId $PIP.Id
 
-# $Credential = New-Object System.Management.Automation.PSCredential ($VMLocalAdminUser, $VMLocalAdminSecurePassword);
-# $VirtualMachine = New-AzureRMVMConfig -VMName $VMName -VMSize $VMSize
-# $VirtualMachine = Set-AzureRMVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName $ComputerName -Credential $Credential -ProvisionVMAgent -EnableAutoUpdate
-# $VirtualMachine = Add-AzureRMVMNetworkInterface -VM $VirtualMachine -Id $NIC.Id
-# $VirtualMachine = Set-AzureRMVMSourceImage -VM $VirtualMachine -PublisherName 'MicrosoftWindowsServer' -Offer 'WindowsServer' -Skus '2016-Datacenter' -Version latest
-# # $VirtualMachine = Set-AzVMBootDiagnostic -VM $VirtualMachine -Disable
+$Credential = New-Object System.Management.Automation.PSCredential ($VMLocalAdminUser, $VMLocalAdminSecurePassword);
+$VirtualMachine = New-AzureRMVMConfig -VMName $VMName -VMSize $VMSize
+$VirtualMachine = Set-AzureRMVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName $ComputerName -Credential $Credential -ProvisionVMAgent -EnableAutoUpdate
+$VirtualMachine = Add-AzureRMVMNetworkInterface -VM $VirtualMachine -Id $NIC.Id
+$VirtualMachine = Set-AzureRMVMSourceImage -VM $VirtualMachine -PublisherName 'MicrosoftWindowsServer' -Offer 'WindowsServer' -Skus '2016-Datacenter' -Version latest
+# $VirtualMachine = Set-AzVMBootDiagnostic -VM $VirtualMachine -Disable
 
-# "Création de la VM"
-# New-AzureRmVM -ResourceGroupName $ResourceGroupName -Location $Location -VM $VirtualMachine -Verbose
+"Création de la VM"
+New-AzureRmVM -ResourceGroupName $ResourceGroupName -Location $Location -VM $VirtualMachine -Verbose
 
-# $DestKey = (get-AzureRmStorageAccountKey -Name gumbackups -ResourceGroupName infrastructure | where-object { $_.keyname -eq "key1" }).value
-# $source = (Get-ChildItem \\srvtfs01\drop\GuichetUnique\ControleQualite\ControleQualite-IC\*\crawler\publish.zip | sort-object -Property creationdate)[0].fullname
-# "Copie du TriggerExecCrawler.zip dans https://gumbackups.blob.core.windows.net/depot-tfs"
-# . $AzCopyPath /source:$source /dest:https://gumbackups.blob.core.windows.net/depot-tfs/TriggerExecCrawler.zip /destkey:$destkey /Y
+$DestKey = (get-AzureRmStorageAccountKey -Name gumbackups -ResourceGroupName infrastructure | where-object { $_.keyname -eq "key1" }).value
+$source = (Get-ChildItem \\srvtfs01\drop\GuichetUnique\ControleQualite\ControleQualite-IC\*\crawler\publish.zip | sort-object -Property creationdate)[0].fullname
+"Copie du TriggerExecCrawler.zip dans https://gumbackups.blob.core.windows.net/depot-tfs"
+. $AzCopyPath /source:$source /dest:https://gumbackups.blob.core.windows.net/depot-tfs/TriggerExecCrawler.zip /destkey:$destkey /Y
 
-# $storageContext = New-AzureStorageContext -StorageAccountName gumbackups -StorageAccountKey $Destkey
-# "Permettre les droits sur le blob https://gumbackups.blob.core.windows.net/depot-tfs"
-# Get-AzureStorageContainer depot-tfs -Context $storageContext | set-AzurestorageContainerAcl -Permission Blob
+$storageContext = New-AzureStorageContext -StorageAccountName gumbackups -StorageAccountKey $Destkey
+"Permettre les droits sur le blob https://gumbackups.blob.core.windows.net/depot-tfs"
+Get-AzureStorageContainer depot-tfs -Context $storageContext | set-AzurestorageContainerAcl -Permission Blob
 
-# "Donne accès à l'adresse IP du crawler au site gummaster"
-# $ip = (Get-AzureRMPublicIpAddress -ResourceGroupName $ResourceGroupName -Name $PublicIPAddressName).ipaddress
-# $Site = "gummaster-$environnement"
-# $APIVersion = ((Get-AzureRMResourceProvider -ProviderNamespace Microsoft.Web).ResourceTypes | Where-Object ResourceTypeName -eq sites).ApiVersions[0]
-# $WebAppConfig = (Get-AzureRMResource -ResourceType Microsoft.Web/sites/config -ResourceName $site -ResourceGroupName GumSite-rg-$Environnement -ApiVersion $APIVersion)
-# $priority = 1;  
-# $IpSecurityRestrictions = $WebAppConfig.Properties.ipsecurityrestrictions; 
-# $IpSecurityRestrictions
+"Donne accès à l'adresse IP du crawler au site gummaster"
+$ip = (Get-AzureRMPublicIpAddress -ResourceGroupName $ResourceGroupName -Name $PublicIPAddressName).ipaddress
+$Site = "gummaster-$environnement"
+$APIVersion = ((Get-AzureRMResourceProvider -ProviderNamespace Microsoft.Web).ResourceTypes | Where-Object ResourceTypeName -eq sites).ApiVersions[0]
+$WebAppConfig = (Get-AzureRMResource -ResourceType Microsoft.Web/sites/config -ResourceName $site -ResourceGroupName GumSite-rg-$Environnement -ApiVersion $APIVersion)
+$priority = 1;  
+$IpSecurityRestrictions = $WebAppConfig.Properties.ipsecurityrestrictions; 
+$IpSecurityRestrictions
 
-# [System.Collections.ArrayList]$ArrayList = $IpSecurityRestrictions ;
+[System.Collections.ArrayList]$ArrayList = $IpSecurityRestrictions ;
 
-# if ($arrayList.ipAddress -notcontains ($Ip + '/32')) {
-#     $webIP = [PSCustomObject]@{ipAddress = ''; action = ''; priority = ""; name = ""; description = ''; }; 
-#     $webip.ipAddress = $ip + '/32';  
-#     $webip.action = "Allow"; 
-#     $webip.name = "Allow_Crawler"
-#     $webIP.priority = $priority;  
-#     $index= $ArrayList.Add($webIP); 
-#     $WebAppConfig.properties.ipSecurityRestrictions = $ArrayList
-#     $WebAppConfig | Set-AzureRMResource -ApiVersion $APIVersion -Force -Verbose
-# }
-# "Attente du rafraichissement du site gum"
+if ($arrayList.ipAddress -notcontains ($Ip + '/32')) {
+    $webIP = [PSCustomObject]@{ipAddress = ''; action = ''; priority = ""; name = ""; description = ''; }; 
+    $webip.ipAddress = $ip + '/32';  
+    $webip.action = "Allow"; 
+    $webip.name = "Allow_Crawler"
+    $webIP.priority = $priority;  
+    $index= $ArrayList.Add($webIP); 
+    $WebAppConfig.properties.ipSecurityRestrictions = $ArrayList
+    $WebAppConfig | Set-AzureRMResource -ApiVersion $APIVersion -Force -Verbose
+}
+"Attente du rafraichissement du site gum"
 # Start-Sleep -s 120
 
 # "Configurer la vm avec Chrome et installer le crawler"
@@ -105,13 +105,13 @@ if ( get-AzureRMResourceGroup -Name $ResourceGroupName -Location $Location -Erro
 # "Done!"
 
 
-# "Retirer les droits sur le blob https://gumbackups.blob.core.windows.net/depot-tfs"
-# Get-AzureStorageContainer depot-tfs -Context $storageContext | set-AzureRMstorageContainerAcl -Permission  Off
+"Retirer les droits sur le blob https://gumbackups.blob.core.windows.net/depot-tfs"
+Get-AzureStorageContainer depot-tfs -Context $storageContext | set-AzureRMstorageContainerAcl -Permission  Off
 
-# "Retire accès à l'adresse IP du crawler au site gummaster"
+"Retire accès à l'adresse IP du crawler au site gummaster"
 
-# $ArrayList.Removeat( $Index ) 
-# $WebAppConfig.properties.ipSecurityRestrictions = $ArrayList
-# $WebAppConfig | Set-AzureRMResource -ApiVersion $APIVersion -Force -Verbose
+$ArrayList.Removeat( $Index ) 
+$WebAppConfig.properties.ipSecurityRestrictions = $ArrayList
+$WebAppConfig | Set-AzureRMResource -ApiVersion $APIVersion -Force -Verbose
 
-# if ( get-AzureRMResourceGroup -Name $ResourceGroupName -Location $Location -ErrorAction SilentlyContinue ) { Remove-AzureRMResourceGroup -Name $ResourceGroupName -Force}
+if ( get-AzureRMResourceGroup -Name $ResourceGroupName -Location $Location -ErrorAction SilentlyContinue ) { Remove-AzureRMResourceGroup -Name $ResourceGroupName -Force}
