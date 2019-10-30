6a5a9d0c942ea1b2e1dc21d12cdc97a0c36a46d5
Yves Germain <ygermain@soquij.local>
2019-10-30 15:39:09 -0400

    Ajout des restrictions sur l'app de Veille et AppsInterne

--- AppsInterne-rg-prd/deploy.ps1
+++ AppsInterne-rg-prd/deploy.ps1
@@ -26,6 +26,7 @@
 
 param(
     [string]
+    [ValidateSet("CanadaCentral", "CanadaEast")] 
     $resourceGroupLocation = "CanadaCentral",
 
     [Parameter()]
@@ -44,6 +45,13 @@ param(
     $Environnement
  
 )
+
+if ( $resourceGroupLocation -eq "CanadaCentral" ) {
+    $IP_logic_Apps = "13.71.184.150", "13.71.186.1", "40.85.250.135", "40.85.250.212", "40.85.252.47", "52.233.29.92", "52.228.39.241", "52.228.39.244" 
+}
+else {
+    $IP_logic_Apps = "40.86.203.228", "40.86.216.241", "40.86.217.241", "40.86.226.149", "40.86.228.93", "52.229.120.45", "52.229.126.25", "52.232.128.155"
+}
 
 <#
 .SYNOPSIS
@@ -112,3 +120,58 @@ if ( $Environnement -eq "dev" -or $Environnement -eq "qa" -or $Environnement -eq
     $dev = Get-AzADGroup -SearchString "dev"
     New-AzRoleAssignment -ObjectId $dev.Id -RoleDefinitionName Owner  -ResourceGroupName $resourceGroupName
 }
+# Mettre les restrictions sur l'app de Veille
+$site = "Veille-" + $Environnement
+
+$APIVersion = ((Get-AzResourceProvider -ProviderNamespace Microsoft.Web).ResourceTypes | Where-Object ResourceTypeName -eq sites).ApiVersions[0]
+$WebAppConfig = (Get-AzResource -ResourceType Microsoft.Web/sites/config -ResourceName $site -ResourceGroupName $resourceGroupName -ApiVersion $APIVersion)
+$priority = 180;  
+$IpSecurityRestrictions = $WebAppConfig.Properties.ipsecurityrestrictions; 
+$IpSecurityRestrictions
+
+[System.Collections.ArrayList]$ArrayList = $IpSecurityRestrictions ;
+
+(Get-azwebapp -name ("AppsInterne-" + $Environnement )).PossibleOutboundIpAddresses.split(",") | ForEach-Object { 
+    $Ip = $_;
+    if ($arrayList.ipAddress -notcontains ($Ip + '/32')) {
+        $webIP = [PSCustomObject]@{ipAddress = ''; action = ''; priority = ""; name = ""; description = ''; }; 
+        $webip.ipAddress = $_ + '/32';  
+        $webip.action = "Allow"; 
+        $webip.name = "Allow_AppsInterne"
+        $priority = $priority + 20 ; 
+        $webIP.priority = $priority;  
+        $ArrayList.Add($webIP); 
+        Remove-Variable webip
+    }
+}
+$WebAppConfig.properties.ipSecurityRestrictions = $ArrayList
+$WebAppConfig | Set-AzResource  -ApiVersion $APIVersion -Force -Verbose
+
+# Mettre les restrictions sur AppsInterne
+
+$site = "AppsInterne-" + $Environnement
+ 
+$APIVersion = ((Get-AzResourceProvider -ProviderNamespace Microsoft.Web).ResourceTypes | Where-Object ResourceTypeName -eq sites).ApiVersions[0]
+$WebAppConfig = (Get-AzResource -ResourceType Microsoft.Web/sites/config -ResourceName $site -ResourceGroupName $resourceGroupName -ApiVersion $APIVersion)
+$priority = 180;  
+$IpSecurityRestrictions = $WebAppConfig.Properties.ipsecurityrestrictions; 
+$IpSecurityRestrictions
+
+[System.Collections.ArrayList]$ArrayList = $IpSecurityRestrictions ;
+
+$IP_logic_Apps | ForEach-Object { 
+    $Ip = $_;
+    if ($arrayList.ipAddress -notcontains ($Ip + '/32')) {
+        $webIP = [PSCustomObject]@{ipAddress = ''; action = ''; priority = ""; name = ""; description = ''; }; 
+        $webip.ipAddress = $_ + '/32';  
+        $webip.action = "Allow"; 
+        $webip.name = "Allow_Logic_App"
+        $priority = $priority + 20 ; 
+        $webIP.priority = $priority;  
+        $ArrayList.Add($webIP); 
+        Remove-Variable webip
+    }
+}
+$WebAppConfig.properties.ipSecurityRestrictions = $ArrayList
+$WebAppConfig | Set-AzResource  -ApiVersion $APIVersion -Force -Verbose
+    
