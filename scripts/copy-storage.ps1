2aa40ecebb298d52626829c414065801a141aaa8
Yves Germain <ygermain@soquij.local>
2019-11-12 16:09:13 -0500

    ,

--- scripts/copy-storage.ps1
+++ scripts/copy-storage.ps1
@@ -19,18 +19,18 @@
     }
 
     $GumBackupKey = (get-azureRMstorageaccountkey -Name gumbackups -ResourceGroupName infrastructure | where-object { $_.keyname -eq "key1" }).value
-    $GumBackupContext = (get-azstorageaccount -name gumbackups -ResourceGroupName Infrastructure).context
 
-    $date = get-date -Format "yyyy-MM-dd"
+    $date = get-date -Format "yyyyMMdd"
 
-    if (!( get-AzStorageContainer -Context $GumBackupContext -name "$storage-$Environnement-$date" -ErrorAction SilentlyContinue) ) {
-        New-AzStorageContainer -Context $GumBackupContext -name "$storage-$Environnement-$date" 
+    if (!( get-AzureRmStorageContainer -resourcegroupName Infrastructure -StorageAccountName gumbackups -name "$container-$Environnement-$date" -ErrorAction SilentlyContinue) ) {
+        New-AzureRmStorageContainer -resourcegroupName Infrastructure -StorageAccountName gumbackups -Name "$container-$Environnement-$date" 
     }
 
-    $newPath = "$storage-$Environnement-$date"
+    $newPath = "$container-$Environnement-$date"
 
-    $SourceKey = ( get-azureRMstorageaccountkey -Name "$storage$Environnement" -ResourceGroupName $ResourceGroupName | where-object { $_.keyname -eq "key1" }).value
+    $SourceKey = ( Get-AzureRmStorageaccountkey -Name "$storage$Environnement" -ResourceGroupName $ResourceGroupName | where-object { $_.keyname -eq "key1" }).value
 
+    Write-Output "copy https://$storage$environnement.blob.core.windows.net/$container vers https://gumbackups.blob.core.windows.net/$newPath"
     . $AzCopyPath /source:https://$storage$environnement.blob.core.windows.net/$container/ /sourcekey:$SourceKey /dest:https://gumbackups.blob.core.windows.net/$newPath/ /s /y /destkey:$GumBackupKey
 }
 
@@ -44,7 +44,7 @@ function restore-storage {
         $Environnement,
     
         [Parameter(Mandatory = $false,
-            HelpMessage = "Donner la date du restore dans le format yyyy-MM-dd, sinon on prendra le dernier backup")]
+            HelpMessage = "Donner la date du restore dans le format yyyyMMdd, sinon on prendra le dernier backup")]
         [datetime]
         $date,
 
@@ -62,20 +62,21 @@ function restore-storage {
     }
 
     $GumBackupKey = (get-azureRMstorageaccountkey -Name gumbackups -ResourceGroupName infrastructure | where-object { $_.keyname -eq "key1" }).value
-    $GumBackupContext = (get-azstorageaccount -name gumbackups -ResourceGroupName Infrastructure).context
 
     $DestKey = (get-azureRMstorageaccountkey -Name "$storage$Environnement" -ResourceGroupName $ResourceGroupName | where-object { $_.keyname -eq "key1" }).value
 
     if (!$date) {
-        $date = ((get-AzStorageContainer -Context $GumBackupContext -name "$storage-$Environnement-*" | sort-object -Property LastModified -Descending)[0]).LastModified.localdatetime.ToShortDateString()
+        $dateformat = ((get-AzureRmStorageContainer -ResourceGroupName infrastructure -StorageAccountName gumbackups | Where-Object {$_.name -like "$container-$Environnement-*"} | sort-object -Property LastModifiedtime -Descending)[0]).LastModifiedtime
+        $date = "{0:yyyyMMdd}" -f $dateformat
     }
 
-    if (!( get-AzStorageContainer -Context $GumBackupContext -name "$storage-$Environnement-$date" -ErrorAction SilentlyContinue)) {
-        write-warning "Le backup n'existe pas";
+
+    if (!( get-AzureRmStorageContainer -ResourceGroupName infrastructure -StorageAccountName gumbackups -name "$container-$Environnement-$date" -ErrorAction SilentlyContinue)) {
+        write-warning "Le backup $container-$Environnement-$date n'existe pas";
         break 
     }
 
-    $newPath = "$storage-$Environnement-$date"
-
+    $newPath = "$container-$Environnement-$date"
+    Write-Output "restore https://gumbackups.blob.core.windows.net/$newPath vers https://$storage$environnement.blob.core.windows.net/$container"
     . $AzCopyPath /source:https://gumbackups.blob.core.windows.net/$newPath/ /sourcekey:$GumBackupKey /dest:https://$storage$environnement.blob.core.windows.net/$container/ /s /y /destkey:$DestKey
 }
