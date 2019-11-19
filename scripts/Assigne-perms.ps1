0c82b46d5ac4ad3a37dce80ad530cc1fd40a711b
Yves Germain <ygermain@soquij.local>
2019-11-19 15:46:09 -0500

    Correction du ConnectionString dans Function App

--- scripts/Assigne-perms.ps1
+++ scripts/Assigne-perms.ps1
@@ -14,13 +14,6 @@ if ( $resourceGroupLocation -eq "CanadaCentral" ) {
     $IP_logic_Apps = "40.86.203.228", "40.86.216.241", "40.86.217.241", "40.86.226.149", "40.86.228.93", "52.229.120.45", "52.229.126.25", "52.232.128.155"
 }
 
-# Donner les droits aux groupes Dev et QA sur les resources groups ***-dev et **-qa
-if ( $Environnement -eq "dev" -or $Environnement -eq "qa") {
-    $QA = Get-AzureRmADGroup -SearchString "QA"
-    New-AzureRmRoleAssignment -ObjectId $QA.Id -RoleDefinitionName Contributor -ResourceGroupName $resourceGroupName -AllowDelegation
-    $dev = Get-AzureRmADGroup -SearchString "dev"
-    New-AzureRmRoleAssignment -ObjectId $dev.Id -RoleDefinitionName Owner  -ResourceGroupName $resourceGroupName -AllowDelegation
-}
 # Mettre les restrictions sur l'app de Veille
 $site = "Veille-" + $Environnement
 
@@ -116,3 +109,20 @@ $IpSecurityRestrictions
 }
 $WebAppConfig.properties.ipSecurityRestrictions = $ArrayList
 Set-AzureRmResource -resourceid $webAppConfig.ResourceId -Properties $WebAppConfig.properties -ApiVersion $APIVersion -Force
+
+$connectionStrings = @{}
+$passw = (Get-AzKeyVaultSecret -VaultName gumkeyvault -name "sqladmin$environnement").secretvaluetext
+$connectionStrings['bd_VeilleContenuExterneEntities'] = @{Type= "SQLServer"; Value= 'Source=sqlguminterne-' + $environnement + '.database.windows.net,1433;Initial Catalog=tbl_VeilleDeContenuExterne;Persist Security Info=False;UserID=sqladmin' + $environnement + ';Password=' + $passw + ';MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;ConnectionTimeout=30;App=EntityFramework'}
+Set-AzWebApp -ResourceGroupName "appsinterne-rg-$environnement" -name "veille-func-$Environnement" -ConnectionStrings $connectionStrings
+
+write-output "Donner les droits aux groupes Dev et QA sur les resources groups ***-dev et **-qa"
+if ( $Environnement -eq "dev" -or $Environnement -eq "qa" -or $Environnement -eq "devops") {
+    $QA = Get-AzureRmADGroup -SearchString "QA"
+    if (!( get-AzureRmRoleAssignment -ResourceGroupName $resourceGroupName -ObjectId $qa.Id -RoleDefinitionName contributor)) {
+        New-AzureRmRoleAssignment -ObjectId $QA.Id -RoleDefinitionName Contributor -ResourceGroupName $resourceGroupName
+    }
+    $dev = Get-AzureRmADGroup -SearchString "dev"
+    if (!( get-AzureRmRoleAssignment -ResourceGroupName $resourceGroupName -ObjectId $dev.Id -RoleDefinitionName owner)) {
+        New-AzureRmRoleAssignment -ObjectId $dev.Id -RoleDefinitionName Owner -ResourceGroupName $resourceGroupName
+    }
+}
