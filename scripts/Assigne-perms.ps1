62fe1e21f0bbbe055cab2ba6b8acc5c704317450
Yves Germain <ygermain@soquij.local>
2019-11-20 08:39:15 -0500

    AzureRM version du script pour mettre le connection string dans Veille Function App

--- scripts/Assigne-perms.ps1
+++ scripts/Assigne-perms.ps1
@@ -110,10 +110,11 @@ $IpSecurityRestrictions
 $WebAppConfig.properties.ipSecurityRestrictions = $ArrayList
 Set-AzureRmResource -resourceid $webAppConfig.ResourceId -Properties $WebAppConfig.properties -ApiVersion $APIVersion -Force
 
+Write-Output "Mettre la Connection String dans l'App Function"
 $connectionStrings = @{}
-$passw = (Get-AzKeyVaultSecret -VaultName gumkeyvault -name "sqladmin$environnement").secretvaluetext
+$passw = (Get-AzureKeyVaultSecret -VaultName gumkeyvault -name "sqladmin$environnement").secretvaluetext
 $connectionStrings['bd_VeilleContenuExterneEntities'] = @{Type= "SQLServer"; Value= 'Source=sqlguminterne-' + $environnement + '.database.windows.net,1433;Initial Catalog=tbl_VeilleDeContenuExterne;Persist Security Info=False;UserID=sqladmin' + $environnement + ';Password=' + $passw + ';MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;ConnectionTimeout=30;App=EntityFramework'}
-Set-AzWebApp -ResourceGroupName "appsinterne-rg-$environnement" -name "veille-func-$Environnement" -ConnectionStrings $connectionStrings
+Set-AzureRmWebApp -ResourceGroupName "appsinterne-rg-$environnement" -name "veille-func-$Environnement" -ConnectionStrings $connectionStrings
 
 write-output "Donner les droits aux groupes Dev et QA sur les resources groups ***-dev et **-qa"
 if ( $Environnement -eq "dev" -or $Environnement -eq "qa" -or $Environnement -eq "devops") {
