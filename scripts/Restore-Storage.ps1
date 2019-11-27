bcf40cd22ad86fe39ba373150377d905dd831534
Yves Germain <ygermain@soquij.local>
2019-11-27 08:12:39 -0500

    Correction de scripts/Restore-Storage.ps1 pour corriger le Environnement et Source

--- scripts/Restore-Storage.ps1
+++ scripts/Restore-Storage.ps1
@@ -1,8 +1,12 @@
 ﻿param(
+    [string]
+    [ValidateSet("dev", "qa", "prd", "devops")] 
+    $Source,
+
     [Parameter(Mandatory = $True)]
     [string]
     [ValidateSet("dev", "qa", "prd", "devops")] 
-    $Environnement,
+    $Destination,
 
     [Parameter(Mandatory = $false,
         HelpMessage = "Donner la date du restore dans le format yyyyMMdd, sinon on prendra le dernier backup")]
@@ -11,18 +15,20 @@
     [Parameter(Mandatory = $True)]
     [string]
     [ValidateSet("storgum", "storappsinterne", "All")] 
-    $storage,
+    $storage
 
-    [string]
-    [ValidateSet("dev", "qa", "prd", "devops")] 
-    $Redirect_To
 )
 function restore-storage {
     param(
+        
+        [string]
+        [ValidateSet("dev", "qa", "prd", "devops")] 
+        $Source,
+
         [Parameter(Mandatory = $True)]
         [string]
         [ValidateSet("dev", "qa", "prd", "devops")] 
-        $Environnement,
+        $Destination,
     
         [Parameter(Mandatory = $false,
             HelpMessage = "Donner la date du restore dans le format yyyyMMdd, sinon on prendra le dernier backup")]
@@ -31,50 +37,43 @@ function restore-storage {
         [Parameter(Mandatory = $True)]
         [string]
         [ValidateSet("storgum", "storappsinterne")] 
-        $storage,
-
-        [string]
-        [ValidateSet("dev", "qa", "prd", "devops")] 
-        $Redirect_To
+        $storage
     )
 
     $AzCopyPath = "C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy\AzCopy.exe"
 
+    if (!$Source ) {$source = $Destination}
+
     switch ($storage) {
-        "storgum" { $ResourceGroupName = "gumstorage-rg-" + $environnement; $container = "guichetunique" }
-        "storappsinterne" { $ResourceGroupName = "Storage-rg-" + $environnement; $container = "appsinterne" }
+        "storgum" { $ResourceGroupName = "gumstorage-rg-" + $Destination; $container = "guichetunique" }
+        "storappsinterne" { $ResourceGroupName = "Storage-rg-" + $Destination; $container = "appsinterne" }
     }
 
     $GumBackupKey = (get-azureRMstorageaccountkey -Name gumbackups -ResourceGroupName infrastructure | where-object { $_.keyname -eq "key1" }).value
 
-    $DestKey = (get-azureRMstorageaccountkey -Name "$storage$Environnement" -ResourceGroupName $ResourceGroupName | where-object { $_.keyname -eq "key1" }).value
+    $DestKey = (get-azureRMstorageaccountkey -Name "$storage$Destination" -ResourceGroupName $ResourceGroupName | where-object { $_.keyname -eq "key1" }).value
 
     if (!$date) {
-        $dateformat = ((get-AzureRmStorageContainer -ResourceGroupName infrastructure -StorageAccountName gumbackups | Where-Object { $_.name -like "$container-$Environnement-*" } | sort-object -Property LastModifiedtime -Descending)[0]).LastModifiedtime
+        $dateformat = ((get-AzureRmStorageContainer -ResourceGroupName infrastructure -StorageAccountName gumbackups | Where-Object { $_.name -like "$container-$Destination-*" } | sort-object -Property LastModifiedtime -Descending)[0]).LastModifiedtime
         $date = "{0:yyyyMMdd}" -f $dateformat
     }
 
-    if (!( get-AzureRmStorageContainer -ResourceGroupName infrastructure -StorageAccountName gumbackups -name "$container-$Environnement-$date" -ErrorAction SilentlyContinue)) {
-        write-warning "Le backup $container-$Environnement-$date n'existe pas";
+    if (!( get-AzureRmStorageContainer -ResourceGroupName infrastructure -StorageAccountName gumbackups -name "$container-$Destination-$date" -ErrorAction SilentlyContinue)) {
+        write-warning "Le backup $container-$Destination-$date n'existe pas";
         break 
     }
     # Pour Rediriger les données vers un autre container.
 
-    if (!$Redirect_To ) {
-        $newPath = "$container-$Environnement-$date"
-    }
-    else {
-        $newPath = "$container-$Redirect_To-$date" 
-    }
+    $newPath = "$container-$Source-$date" 
     
-    Write-Output "restore https://gumbackups.blob.core.windows.net/$newPath vers https://$storage$environnement.blob.core.windows.net/$container"
-    . $AzCopyPath /source:https://gumbackups.blob.core.windows.net/$newPath/ /sourcekey:$GumBackupKey /dest:https://$storage$environnement.blob.core.windows.net/$container/ /s /y /destkey:$DestKey
+    Write-Output "restore https://gumbackups.blob.core.windows.net/$newPath vers https://$storage$Destination.blob.core.windows.net/$container"
+    . $AzCopyPath /source:https://gumbackups.blob.core.windows.net/$newPath/ /sourcekey:$GumBackupKey /dest:https://$storage$Destination.blob.core.windows.net/$container/ /s /y /destkey:$DestKey
 }
 
-$params = @{'Environnement' = $Environnement }
+$params = @{'Environnement' = $Destination }
 
 if ($PSBoundParameters.ContainsKey('Date')) { $params.Add('Date', $Date) }
-if ($PSBoundParameters.ContainsKey('Redirect_To')) { $params.Add('Redirect_To', $Redirect_To) }
+if ($PSBoundParameters.ContainsKey('$Source')) { $params.Add('$Source', $Source) }
 
 if ($storage -eq "All") {
     $storage = "storgum", "storappsinterne";
@@ -86,8 +85,8 @@ foreach ($store in $Storage) {
 
     Write-output "Copier la clef du Storage Account dans Gum Key Vault"
     switch ($store) {
-        "storgum" { $ResourceGroupName = "gumstorage-rg-" + $environnement; $StorageAccounts = "storgum" }
-        "storappsinterne" { $ResourceGroupName = "Storage-rg-" + $environnement; $StorageAccounts = "storappsinterne" , "storveillefunc" }
+        "storgum" { $ResourceGroupName = "gumstorage-rg-" + $Destination; $StorageAccounts = "storgum" }
+        "storappsinterne" { $ResourceGroupName = "Storage-rg-" + $Destination; $StorageAccounts = "storappsinterne" , "storveillefunc" }
     }
     foreach ($StorageAccount in $StorageAccounts) {
         get-azureRmstorageaccount -resourcegroupName $resourceGroupName | where-object { $_.storageaccountname -like "$StorageAccount*" } | foreach-object { 
