9fe62cbec970f78945dfac5aa9e0d19b72586e3e
Yves Germain <ygermain@soquij.local>
2019-12-05 09:09:00 -0500

    test de $chromepath2

--- AppsCrawler-rg-prd/Install-Chrome2.ps1
+++ AppsCrawler-rg-prd/Install-Chrome2.ps1
@@ -6,34 +6,9 @@ Param(
     $Environnement 
 )
 
-$LocalTempDir = $env:TEMP; 
-"Installation de nodejs"
-$NODEJS_FILENAME="node-v13.2.0-x64.msi"
-$NODEJS_URL= "https://nodejs.org/dist/v13.2.0/$NODEJS_FILENAME"
-$NODEJS_DOWNLOAD_LOCATION= "C:\"
-
-(New-Object Net.WebClient).DownloadFile($NODEJS_URL, "$NODEJS_DOWNLOAD_LOCATION$NODEJS_FILENAME"); 
-msiexec /qn /l* C:\node-log.txt /i "$NODEJS_DOWNLOAD_LOCATION$NODEJS_FILENAME"
-
-"Starting";
-"Installation de Chrome"
-
-$ChromeInstaller = "ChromeInstaller.exe"; 
-(new-object    System.Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', "$LocalTempDir\$ChromeInstaller");
-& "$LocalTempDir\$ChromeInstaller" /silent /install;
-"Downloading TriggerExecCrawler.zip"
-(new-object System.Net.WebClient).DownloadFile('https://gumbackups.blob.core.windows.net/depot-tfs/TriggerExecCrawler.zip', "$env:temp\TriggerExecCrawler.zip");
-
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-
-set-location "C:\Program Files\nodejs"
-npm install -g lighthouse
-
-"Decompressing file TriggerExecCrawler.zip in c:\crawler"
-Expand-Archive -LiteralPath "$env:temp\TriggerExecCrawler.zip" -DestinationPath C:\crawler
 Get-ChildItem C:\crawler\*\ControleQualite.App.exe | foreach-object {set-location $_.DirectoryName}
 (Get-Content ControleQualite.App.exe.config ).replace('gummaster-dev' , "gummaster-$environnement") | set-content .\ControleQualite.App.exe.config -Encoding UTF8
-# .\ControleQualite.App.exe
+.\ControleQualite.App.exe
 
 "Done!"
 get-process chromedriver | stop-process -Force
