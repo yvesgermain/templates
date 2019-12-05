c49b512acc9036fb154fbb19570310ef134e57e7
Yves Germain <ygermain@soquij.local>
2019-12-05 09:39:28 -0500

    test de $chromepath2

--- AppsCrawler-rg-prd/Install-Chrome.ps1
+++ AppsCrawler-rg-prd/Install-Chrome.ps1
@@ -33,7 +33,7 @@ npm install -g lighthouse
 Expand-Archive -LiteralPath "$env:temp\TriggerExecCrawler.zip" -DestinationPath C:\crawler
 Get-ChildItem C:\crawler\*\ControleQualite.App.exe | foreach-object {set-location $_.DirectoryName}
 (Get-Content ControleQualite.App.exe.config ).replace('gummaster-dev' , "gummaster-$environnement") | set-content .\ControleQualite.App.exe.config -Encoding UTF8
-.\ControleQualite.App.exe
+# .\ControleQualite.App.exe
 
 "Done!"
 get-process chromedriver | stop-process -Force
