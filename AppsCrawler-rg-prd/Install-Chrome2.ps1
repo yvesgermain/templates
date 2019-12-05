7897e6c17ccbda7ec2f8c6b67d6ba487f5e93aad
Yves Germain <ygermain@soquij.local>
2019-12-05 12:39:33 -0500

    test de $chromepath avec where

--- AppsCrawler-rg-prd/Install-Chrome2.ps1
+++ AppsCrawler-rg-prd/Install-Chrome2.ps1
@@ -8,7 +8,7 @@ Param(
 
 Get-ChildItem C:\crawler\*\ControleQualite.App.exe | foreach-object {set-location $_.DirectoryName}
 (Get-Content ControleQualite.App.exe.config ).replace('gummaster-dev' , "gummaster-$environnement") | set-content .\ControleQualite.App.exe.config -Encoding UTF8
-.\ControleQualite.App.exe
+cmd /c ControleQualite.App.exe
 
 "Done!"
 get-process chromedriver | stop-process -Force
