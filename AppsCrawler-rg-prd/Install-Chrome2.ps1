269be2fd51be50eb38cc2a10b7f0692b02ec314c
Yves Germain <ygermain@soquij.local>
2019-12-06 12:05:30 -0500

    AppsCrawler-rg-prd/Install-Chrome2.ps1 $dir = Get-ChildItem C:\crawler\*\ControleQualite.App.exe!

--- AppsCrawler-rg-prd/Install-Chrome2.ps1
+++ AppsCrawler-rg-prd/Install-Chrome2.ps1
@@ -12,7 +12,8 @@ set-location "C:\Program Files\nodejs"
 .\npm install -g lighthouse --loglevel verbose >> c:\log.log
 
 Write-output "Starting Crawler" ;
-Get-ChildItem C:\crawler\*\ControleQualite.App.exe | foreach-object {set-location $_.DirectoryName}
+$dir = Get-ChildItem C:\crawler\*\ControleQualite.App.exe 
+set-location $dir.DirectoryName
 (Get-Content ControleQualite.App.exe.config ).replace('gummaster-dev' , "gummaster-$environnement") | set-content .\ControleQualite.App.exe.config -Encoding UTF8
 .\ControleQualite.App.exe
 
