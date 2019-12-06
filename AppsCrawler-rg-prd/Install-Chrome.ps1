26f346ce1c4b207faa678a175e8db270d6ee4cca
Yves Germain <ygermain@soquij.local>
2019-12-06 12:37:34 -0500

    AppsCrawler-rg-prd/Install-Chrome2.ps1 $dir = Get-ChildItem C:\crawler\*\ControleQualite.App.exe!

--- AppsCrawler-rg-prd/Install-Chrome.ps1
+++ AppsCrawler-rg-prd/Install-Chrome.ps1
@@ -36,11 +36,9 @@ Write-output "Downloading TriggerExecCrawler.zip"
 Expand-Archive -LiteralPath "$env:temp\TriggerExecCrawler.zip" -DestinationPath C:\crawler
 Get-ChildItem C:\crawler\*\ControleQualite.App.exe | foreach-object {set-location $_.DirectoryName}
 (Get-Content ControleQualite.App.exe.config ).replace('gummaster-dev' , "gummaster-$environnement") | set-content .\ControleQualite.App.exe.config -Encoding UTF8
+
 <#
-
 .\ControleQualite.App.exe
-
 Write-output "Done controleQualiteApp.exe !"
 get-process chromedriver | stop-process -Force
-
 #>
