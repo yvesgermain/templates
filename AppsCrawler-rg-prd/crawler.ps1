403b47174b4a5ddf423955675e979a37fb21a609
Yves Germain <ygermain@soquij.local>
2019-12-09 11:06:43 -0500

    Rajout du Publish.zip et augmenter le delai = 60 secondes et enlever l'adresse IP de Gummaster et detruire le resourcegroup

--- AppsCrawler-rg-prd/crawler.ps1
+++ AppsCrawler-rg-prd/crawler.ps1
@@ -106,10 +106,9 @@ Invoke-AzureRMVMRunCommand -ResourceGroupName $ResourceGroupName -VMName $VmName
 "Retirer les droits sur le blob https://gumbackups.blob.core.windows.net/depot-tfs"
 Get-AzureStorageContainer depot-tfs -Context $storageContext | set-AzurestorageContainerAcl -Permission  Off
 
-<#"Retire accès à l'adresse IP du crawler au site gummaster"
+"Retire accès à l'adresse IP du crawler au site gummaster"
 
 $WebAppConfig.properties.ipSecurityRestrictions = $IpSecurityRestrictions
 Set-AzureRmResource -resourceid $webAppConfig.ResourceId -Properties $WebAppConfig.properties -ApiVersion $APIVersion -Force
 
 if ( get-AzureRMResourceGroup -Name $ResourceGroupName -Location $Location -ErrorAction SilentlyContinue ) { Remove-AzureRMResourceGroup -Name $ResourceGroupName -Force }
-#>
