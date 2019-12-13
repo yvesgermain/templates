6d2bc76289914195cb992e104b05a38ace2d5a6c
Yves Germain <ygermain@soquij.local>
2019-12-13 15:36:13 -0500

    User bearer token pour TFS

--- scripts/git-clone.ps1
+++ scripts/git-clone.ps1
@@ -2,7 +2,7 @@ import-module vsteam
 Set-VSTeamAccount -Account http://srvtfs01:8080/tfs/soquij -UseBearerToken 'aeysyml9sQfogRSS+vQHfUoLEgpfRPOf4hQoPdjZYO4'
 Set-VSTeamDefaultProject -Project GuichetUnique
 whoami.exe 
-$id = (Get-VSTeamReleaseDefinition -ProjectName GuichetUnique | where-object { $_.name -like "Infrastructure Azure Guichet Unique" }).id
+Get-VSTeamReleaseDefinition -ProjectName GuichetUnique -verbose | where-object { $_.name -like "Infrastructure Azure Guichet Unique" } 
 
 # $Release = Add-VSTeamRelease -ArtifactAlias devops -ProjectName guichetUnique -BuildId $BuildId -DefinitionId $id
 # Show-VSTeamRelease -ProjectName GuichetUnique -id $Release.id
