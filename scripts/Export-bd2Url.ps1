705416089d136125df8b45b16f09430a4dd78cdb
Yves Germain <ygermain@soquij.local>
2020-01-09 14:12:28 -0500

    test variable op

--- scripts/Export-bd2Url.ps1
+++ scripts/Export-bd2Url.ps1
@@ -34,5 +34,9 @@ foreach ( $server in $Servers ) {
     $databases = get-azureRMsqlserver | where-object {$_.ServerName -eq $server} | get-azureRMsqldatabase | where-object { $_.Databasename -notlike "master" } 
     $databases | ForEach-Object {$op += New-azureRMSqlDatabaseExport -ServerName $_.servername -DatabaseName $_.databasename -ResourceGroupName $_.ResourceGroupName -StorageKey $storageKey -StorageKeyType $StorageAccessKey -StorageUri $( $TargetUrl + $_.DatabaseName + "_" + $(get-date -Format "yyyy-MM-dd_HH-mm") + '.bacpac' ) -AdministratorLogin $AdministratorLogin -AdministratorLoginPassword $pass}
 }
+write-output "##vso[task.setvariable variable=op]$op"
+# $op.operationstatuslink | ForEach-Object { do { $status = Get-AzurermSqlDatabaseImportExportStatus -OperationStatusLink $_ ; $status; "Sleep for 20 seconds" ; Start-Sleep -Seconds 20}  while ( $status.status -notlike "Succeeded")}
 
-$op.operationstatuslink | ForEach-Object { do { $status = Get-AzurermSqlDatabaseImportExportStatus -OperationStatusLink $_ ; $status; "Sleep for 20 seconds" ; Start-Sleep -Seconds 20}  while ( $status.status -notlike "Succeeded")}
+$env:op
+
+$env:SYSTEM_TEAMFOUNDATIONSERVERURI                                                        
