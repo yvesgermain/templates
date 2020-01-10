3ace47e08e32772fb0acb250ba263d4b9b805972
Yves Germain <ygermain@soquij.local>
2020-01-10 13:25:36 -0500

    Ajout de la bd de gum dans copy-database.ps1

--- scripts/copy-database.ps1
+++ scripts/copy-database.ps1
@@ -8,40 +8,54 @@
 
 #>
 Param(
-        [Parameter(Mandatory=$true)]
-        [ValidateSet("dev", "qa", "prd", "devops")]
-        [string]
-        $SourceEnv = "prd",
+    [Parameter(Mandatory = $true)]
+    [ValidateSet("dev", "qa", "prd", "devops")]
+    [string]
+    $SourceEnv = "prd",
 
-        [Parameter(Mandatory=$true)]
-        [ValidateSet("dev", "qa", "prd", "devops")]
-        [string]
-        $TargetEnv = "devops",
+    [Parameter(Mandatory = $true)]
+    [ValidateSet("dev", "qa", "prd", "devops")]
+    [string]
+    $TargetEnv = "devops",
 
-        [Parameter()]
-        [validateset("BdAppsInterne", "BdVeille", {"BdAppsInterne", "BdVeille"})]
-        [string[]] 
-        $BdArray = ("BdAppsInterne","BdVeille")
-    )
-import-module azurerm.sql, azurerm.Websites
+    [Parameter()]
+    [validateset("BdAppsInterne", "BdVeille", "BdGum", {@('BdAppsInterne','BdVeille')}, {@('BDGum','BdAppsInterne','BdVeille')})]
+    [array] 
+    $BdArray = ("BdAppsInterne", "BdVeille")
+)
+if ( get-module az.sql) { import-module az.sql, az.Websites } else { import-module azurerm.sql, azurerm.Websites } 
 
- foreach( $Bd in $Bdarray) {
-   $databaseName = $Bd + "-" + $SourceEnv
-   $serverName = "sqlguminterne-" + $SourceEnv
-   $resourceGroupName = "sqlapps-rg-" +  $SourceEnv
+foreach ( $Bd in $Bdarray) {
+    if ( $Bd -like "BdGum") {
+        $databaseName = $Bd + "-" + $SourceEnv
+        $serverName = "sqlgum-" + $SourceEnv
+        $resourceGroupName = "GumSql-rg-" + $SourceEnv
+        
+        $TargetDatabaseName = $Bd + "-" + $TargetEnv
+        $TargetServerName = "sqlgum-" + $TargetEnv
+        $TargetResourceGroupName = "GumSql-rg-" + $TargetEnv
+    }   else {
+        $databaseName = $Bd + "-" + $SourceEnv
+        $serverName = "sqlguminterne-" + $SourceEnv
+        $resourceGroupName = "sqlapps-rg-" + $SourceEnv
+        
+        $TargetDatabaseName = $Bd + "-" + $TargetEnv
+        $TargetServerName = "sqlguminterne-" + $TargetEnv
+        $TargetResourceGroupName = "sqlapps-rg-" + $TargetEnv
+    }
 
-   $TargetDatabaseName = $Bd + "-" + $TargetEnv
-   $TargetServerName = "sqlguminterne-" + $TargetEnv
-   $TargetResourceGroupName = "sqlapps-rg-" +  $TargetEnv
+    "Removing database $TargetDatabaseName"
+    if (get-azurermSqlDatabase -DatabaseName $TargetDatabaseName -ServerName $TargetServerName -ResourceGroupName $TargetResourceGroupName -ErrorAction SilentlyContinue) {
+        Remove-azurermSqlDatabase -DatabaseName $TargetDatabaseName -ServerName $TargetServerName -ResourceGroupName $TargetResourceGroupName
+    }
+    "Copying database  $databaseName  from server $servername to database $TargetDatabaseName on $TargetServerName"
+    New-azurermSqlDatabaseCopy -ServerName $serverName -ResourceGroupName $resourceGroupName -DatabaseName $databaseName `
+        -CopyResourceGroupName $TargetResourceGroupName -CopyServerName $TargetServerName -CopyDatabaseName $TargetDatabaseName
 
-   "Removing database $TargetDatabaseName"
-   if (get-azurermSqlDatabase -DatabaseName $TargetDatabaseName -ServerName $TargetServerName -ResourceGroupName $TargetResourceGroupName -ErrorAction SilentlyContinue) {
-   Remove-azurermSqlDatabase -DatabaseName $TargetDatabaseName -ServerName $TargetServerName -ResourceGroupName $TargetResourceGroupName
-   }
-   "Copying database  $databaseName  from server $servername to database $TargetDatabaseName on $TargetServerName"
-   New-azurermSqlDatabaseCopy -ServerName $serverName -ResourceGroupName $resourceGroupName -DatabaseName $databaseName `
-   -CopyResourceGroupName $TargetResourceGroupName -CopyServerName $TargetServerName -CopyDatabaseName $TargetDatabaseName
+    "Redémarrer le site web AppsInterne-rg-$TargetEnv"
+    switch ($Bd) { 
+        "BdAppsInterne" { Restart-azurermWebApp -Name "Appsinterne-$TargetEnv" -ResourceGroupName "AppsInterne-rg-$TargetEnv" }
+        "BdVeille" { Restart-azurermWebApp -Name "Veille-$TargetEnv" -ResourceGroupName "AppsInterne-rg-$TargetEnv" } ;
+        "BdGum" { Restart-azurermWebApp -Name "Gum-$TargetEnv" -ResourceGroupName "GumSite-$TargetEnv"; Restart-azurermWebApp -Name "GumMaster-$TargetEnv" -ResourceGroupName "GumSite-$TargetEnv" }
+    }
 }
-
-"Redémarrer le site web AppsInterne-rg-$TargetEnv"
-Restart-azurermWebApp -Name Appsinterne-$TargetEnv -ResourceGroupName AppsInterne-rg-$TargetEnv
