becb0a09568056ff81ff39ec82447ac8c8fb8abe
Yves Germain <ygermain@soquij.qc.ca>
2020-02-12 15:16:02 -0500

    scripts/kudu_function.ps1

--- scripts/kudu_function.ps1
+++ scripts/kudu_function.ps1
@@ -6,8 +6,8 @@ param(
     $DefaultWorkingDirectory
 )
 . $DefaultWorkingDirectory/DevOps/scripts/Functions.ps1;
-$resourcegroupname = "gumsite-rg-dev";
-$webappname = "gum-dev";
+$resourcegroupname = "gumsite-rg-$environnement";
+$webappname = "gum-$environnement";
 $kudupath = 'App_Data/Logs/' ; 
 $localpath = "c:\temp\logskudu\"; 
 $a = Read-FilesFromWebApp -resourceGroupName $resourceGroupName -webAppName $webAppName -kuduPath $kuduPath; 
