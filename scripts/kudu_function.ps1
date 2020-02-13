f713f2a62039aca50e7310f630e501411f50feb1
Yves Germain <ygermain@soquij.qc.ca>
2020-02-12 21:45:06 -0500

    Kudu fixed!

--- scripts/kudu_function.ps1
+++ scripts/kudu_function.ps1
@@ -5,19 +5,25 @@ param(
     $environnement,
     [Parameter(Mandatory = $True)]
     [string]
-    [ValidateSet("Gum","gummaster", "AppsInterne")] 
-    $web,
+    [ValidateSet("Gum", "AppsInterne")] 
+    $Domaine,
     $DefaultWorkingDirectory
 )
 . $DefaultWorkingDirectory/DevOps/scripts/Functions.ps1;
-if ($web -like "AppsInterne") {$ResourceGroupName = "AppsInterne-rg-$environnement"} else { $ResourceGroupName = "gumsite-rg-$environnement"};
-$webappname = "$web-$environnement";
+if ($web -like "AppsInterne") {
+    $ResourceGroupName = "AppsInterne-rg-$environnement"; $webappnames = "Appsinterne-$environnement"
+} else { 
+    $ResourceGroupName = "gumsite-rg-$environnement"; $webappnames = "Gum-$environnement", "Gummaster-$Environnement"
+};
+
 $kudupath = 'App_Data/Logs/' ; 
 $localpath = "c:\temp\logskudu\"; 
-$Result = try { Read-FilesFromWebApp -resourceGroupName $resourceGroupName -webAppName $webAppName -kuduPath $kuduPath } catch [System.SystemException] { }; 
-if (!$Result) {
-    $Result | ForEach-Object {
-        $name = $_.name;
-        if (!( Test-path "$localPath$webappname\" )) {mkdir "$localPath$webappname"}
-        Read-FilesFromWebApp -resourceGroupName $resourceGroupName -webAppName $webAppName -kuduPath $("$kuduPath$name") -localPath $("$localPath$webappname\$name") }
+Foreach ($webappname in $webappnames) {
+    $Result = try { Get-FileFromWebApp -resourceGroupName $resourceGroupName -webAppName $webAppName -kuduPath $kuduPath } catch [System.SystemException] { }; 
+    if ($Result) {
+        $Result | ForEach-Object {
+            $name = $_.name;
+            if (!( Test-path "$localPath$webappname\" )) { mkdir "$localPath$webappname" }
+            Read-FilesFromWebApp -resourceGroupName $resourceGroupName -webAppName $webAppName -kuduPath $("$kuduPath$name") -localPath $("$localPath$webappname\$name") }
+    }
 }
