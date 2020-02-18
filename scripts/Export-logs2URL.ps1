f54c85fa4a38185964efe09e8a4fb77bb6b3199b
Yves Germain <ygermain@soquij.qc.ca>
2020-02-17 23:37:32 -0500

    scripts/Export-logs2URL.ps1

--- scripts/Export-logs2URL.ps1
+++ scripts/Export-logs2URL.ps1
@@ -14,8 +14,7 @@ param(
 if ($env:COMPUTERNAME -like "srvtfs01") { . "$DefaultWorkingDirectory\DevOps\scripts\Functions.ps1" }  else { . C:\templates\DevOps\scripts\Functions.ps1 }
 if ($Domaine -like "AppsInterne") {
     $ResourceGroupName = "AppsInterne-rg-$environnement"; $webappnames = "Appsinterne-$environnement"
-}
-else { 
+} else { 
     $ResourceGroupName = "gumsite-rg-$environnement"; $webappnames = "Gum-$environnement", "Gummaster-$Environnement"
 };
 
@@ -35,14 +34,10 @@ Foreach ($webappname in $webappnames) {
             if (!( Test-path "$localPath$webappname\" )) { mkdir "$localPath$webappname" }
             # Copying $name in $localPath$webappname
             Read-FilesFromWebApp -resourceGroupName $resourceGroupName -webAppName $webAppName -kuduPath $("$kuduPath$name") -localPath $("$localPath$webappname\$name") }
-    }
-    else { "Rien a sauver!" }
-
-    $Container = "$webappname$(get-date -Format `"yyyy-MM-dd`")".ToLower()
-    "New-AzureRmStorageContainer -Context $context.context -Name $Container"
-
-    New-AzureRmStorageContainer -resourcegroupName Infrastructure -StorageAccountName gumlogs -Name $Container
-
-    & $AzCopyPath /Source:"$localPath$webappname" /Dest:"https://gumlogs.blob.core.windows.net/$Container" /DestKey:$key /S /Y
+        $Container = "$webappname$(get-date -Format `"yyyy-MM-dd`")".ToLower()
+        "New-AzureRmStorageContainer -Context $context.context -Name $Container"
+        New-AzureRmStorageContainer -resourcegroupName Infrastructure -StorageAccountName gumlogs -Name $Container
+        & $AzCopyPath /Source:"$localPath$webappname" /Dest:"https://gumlogs.blob.core.windows.net/$Container" /DestKey:$key /S /Y
+    } else { "Rien a sauver!" }
 }
 remove-item $localpath -Recurse -Force -Confirm:$false
