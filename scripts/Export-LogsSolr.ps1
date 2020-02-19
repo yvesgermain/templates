07bea4e4e465aa9b2055c60c8985c4d6ea5eb925
Yves Germain <ygermain@soquij.qc.ca>
2020-02-19 12:33:42 -0500

    timeZone: Eastern Standard Time

--- scripts/Export-LogsSolr.ps1
+++ scripts/Export-LogsSolr.ps1
@@ -19,3 +19,13 @@ param(
 )
 . "$DefaultWorkingDirectory\DevOps\scripts\Functions.ps1"
 Compress-kudufolder -Environnement prd -Method $method -SiteWeb GumSolr -kuduPath "server/solr/" -DefaultWorkingDirectory $DefaultWorkingDirectory
+
+$AzCopyPath = "C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy\AzCopy.exe"
+"Getting Azure Gumlogs key"
+$key = (Get-AzureRmStorageAccountKey -Name gumlogs -ResourceGroupName Infrastructure )[0].value
+
+if ($method = "Get") {
+$Container = "$SiteWeb$(get-date -Format `"yyyy-MM-dd`")".ToLower()
+& $AzCopyPath /Source:$InFile /Dest:"https://gumlogs.blob.core.windows.net/$Container/$infile" /DestKey:$key /Y
+}
+
