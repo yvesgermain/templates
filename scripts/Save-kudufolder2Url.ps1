0b1850e9361bcfdf447a53dbe836bfb1236a95bb
Yves Germain <ygermain@soquij.qc.ca>
2020-02-21 20:44:56 -0500

    scripts/Save-kudufolder2Url.ps1

--- scripts/Save-kudufolder2Url.ps1
+++ scripts/Save-kudufolder2Url.ps1
@@ -24,7 +24,7 @@ if (!( Get-PSDrive -name z -ErrorAction SilentlyContinue)) {new-azdrive}
 $WebappName = "$webapp-$Environnement"
 $resourceGroupName = (get-azureRmwebapp -name $webAppName ).resourcegroup
 $ZipFilePath = ($ZipFolder + ":\" + $environnement + "\" + $webAppName + (get-date -Format "yyyy-MM-dd") +  ".zip") 
-stop-azurermwebapp -resourceGroupName $resourceGroupName -name $WebappName 
+# stop-azurermwebapp -resourceGroupName $resourceGroupName -name $WebappName 
 Compress-KuduFolderToZipFile -Environnement $environnement -resourceGroupName $resourceGroupName -webAppName $webAppName -ZipFilePath $ZipFilePath -kuduPath $kuduPath 
 Get-ChildItem ($ZipFolder + ":\" + $environnement) | Where-Object {$_.LastWriteTime -lt (get-date).adddays(-10)} | remove-item
 remove-psdrive -name $ZipFolder
