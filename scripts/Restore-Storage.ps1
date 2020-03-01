31c864b4e34e3290714b1ab1e988dcbf4d26248e
Yves Germain <ygermain@soquij.qc.ca>
2020-03-01 02:31:27 -0500

    test AzureRM vers AZ

--- scripts/Restore-Storage.ps1
+++ scripts/Restore-Storage.ps1
@@ -41,7 +41,7 @@ function restore-storage {
         $storage
     )
     if ($storage -eq "null") { Write-Output "Storage = null. Aucun storage à restaurer!"; return 0 }
-    $AzCopyPath = "C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy\AzCopy.exe"
+    $AzCopyPath = "C:\Program Files (x86)\Microsoft SDKs\Az\AzCopy\AzCopy.exe"
 
     if (!$Source ) { $source = $Destination }
 
@@ -50,16 +50,16 @@ function restore-storage {
         "storappsinterne" { $ResourceGroupName = "AppsStorage-rg-" + $Destination; $container = "appsinterne" }
     }
 
-    $GumBackupKey = (get-azureRMstorageaccountkey -Name gumbackups -ResourceGroupName infrastructure | where-object { $_.keyname -eq "key1" }).value
+    $GumBackupKey = (get-Azstorageaccountkey -Name gumbackups -ResourceGroupName infrastructure | where-object { $_.keyname -eq "key1" }).value
 
-    $DestKey = (get-azureRMstorageaccountkey -Name "$storage$Destination" -ResourceGroupName $ResourceGroupName | where-object { $_.keyname -eq "key1" }).value
-    if (!( get-AzureRmStorageContainer -ResourceGroupName infrastructure -StorageAccountName gumbackups | where-object { $_.name -like "$container-$Source-$date*" } -ErrorAction SilentlyContinue)) {
+    $DestKey = (get-Azstorageaccountkey -Name "$storage$Destination" -ResourceGroupName $ResourceGroupName | where-object { $_.keyname -eq "key1" }).value
+    if (!( get-AzStorageContainer -ResourceGroupName infrastructure -StorageAccountName gumbackups | where-object { $_.name -like "$container-$Source-$date*" } -ErrorAction SilentlyContinue)) {
         write-warning "Le backup $container-$Source-$date n'existe pas";
         break 
     }
 
     if (!$date) {
-        $dateformat = ((get-AzureRmStorageContainer -ResourceGroupName infrastructure -StorageAccountName gumbackups | Where-Object { $_.name -like "$container-$Source-*" } | sort-object -Property LastModifiedtime -Descending)[0]).LastModifiedtime.ToLocalTime()
+        $dateformat = ((get-AzStorageContainer -ResourceGroupName infrastructure -StorageAccountName gumbackups | Where-Object { $_.name -like "$container-$Source-*" } | sort-object -Property LastModifiedtime -Descending)[0]).LastModifiedtime.ToLocalTime()
         $date = "{0:yyyyMMdd}" -f $dateformat
     }
 
@@ -91,11 +91,11 @@ foreach ($store in $Storage) {
         "storappsinterne" { $ResourceGroupName = "AppsStorage-rg-" + $Destination; $StorageAccounts = "storappsinterne" , "storveillefunc" }
     }
     foreach ($StorageAccount in $StorageAccounts) {
-        get-azureRmstorageaccount -resourcegroupName $resourceGroupName | where-object { $_.storageaccountname -like "$StorageAccount*" } | foreach-object { 
+        get-Azstorageaccount -resourcegroupName $resourceGroupName | where-object { $_.storageaccountname -like "$StorageAccount*" } | foreach-object { 
             $name = $_.StorageAccountName;
-            Get-AzureRmStorageAccountKey -ResourceGroupName $_.resourcegroupname -Name $Name } | where-object { $_.keyname -like "key1" } | ForEach-Object {
+            Get-AzStorageAccountKey -ResourceGroupName $_.resourcegroupname -Name $Name } | where-object { $_.keyname -like "key1" } | ForEach-Object {
             $Secret = ConvertTo-SecureString -String $_.value -AsPlainText -Force; 
-            Set-AzureKeyVaultSecret -VaultName 'gumkeyvault' -Name $name -SecretValue $Secret -ContentType "Storage key"
+            Set-AzKeyVaultSecret -VaultName 'gumkeyvault' -Name $name -SecretValue $Secret -ContentType "Storage key"
         }
     }
 }
